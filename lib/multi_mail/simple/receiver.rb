3b8ffcc09bee9939ee682a87e2d0a35760d47a9a
James McKinney <james@slashpoundbang.com>
2013-03-03 17:57:19 -0500

    describe simple receiver better

--- lib/multi_mail/simple/receiver.rb
+++ lib/multi_mail/simple/receiver.rb
@@ -2,7 +2,8 @@ module MultiMail
   module Receiver
     class Simple < MultiMail::Service
       include MultiMail::Receiver::Base
-      # Transforms the content of a simple webhook into a list of messages.
+      # Expects the value of the "message" query string parameter to be a raw
+      # email message parsable by the Mail gem.
       #
       # @param [Hash] params the content of the webhook
       # @return [Array<Mail::Message>] messages
