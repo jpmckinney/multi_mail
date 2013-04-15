3e55e63b1383b4a43982354d080c708abea0577f
James McKinney <james@slashpoundbang.com>
2013-04-15 04:34:52 -0400

    finish cloudmailin multipart specs

--- spec/cloudmailin/receiver_spec.rb
+++ spec/cloudmailin/receiver_spec.rb
@@ -37,31 +37,30 @@ describe MultiMail::Receiver::Cloudmailin do
 
       describe '#transform' do
         it 'should return a mail message' do
-          pending
           message = service.transform(params('valid'))[0]
 
           # Headers
-          message.date.should    == DateTime.parse('Mon, 14 Apr 2013 20:55:30 -04:00')
+          message.date.should    == DateTime.parse('Mon, 15 Apr 2013 03:20:46 -04:00')
           message.from.should    == ['james@opennorth.ca']
-          message.to.should      == ['foo+bar@multimail.mailgun.org']
+          message.to.should      == ['5dae6f85cd65d30d384a@cloudmailin.net']
           message.subject.should == 'Test'
 
           # Body
           message.multipart?.should            == true
           message.parts.size.should            == 4
           message.parts[0].content_type.should == 'text/plain'
-          message.parts[0].body.should         == "bold text\n\n> multiline\n> quoted\n> text\n\n\n--\nSignature block\n"
+          message.parts[0].body.should         == "bold text\n\n> multiline\n> quoted\n> text\n\n\n--\nSignature block"
           message.parts[1].content_type.should == 'text/html; charset=UTF-8'
-          message.parts[1].body.should         == %(<html><head></head><body style="word-wrap: break-word; -webkit-nbsp-mode: space; -webkit-line-break: after-white-space; "><b>bold text</b><div><br></div><div><blockquote type="cite">multiline</blockquote><blockquote type="cite">quoted</blockquote><blockquote type="cite">text</blockquote></div><div><br></div><div>--</div><div>Signature block</div><div></div></body></html><html><body style="word-wrap: break-word; -webkit-nbsp-mode: space; -webkit-line-break: after-white-space; "><head></head><div></div></body></html><html><head></head><body style="word-wrap: break-word; -webkit-nbsp-mode: space; -webkit-line-break: after-white-space; "><div></div></body></html>)
+          message.parts[1].body.should         == %(<html><head></head><body style="word-wrap: break-word; -webkit-nbsp-mode: space; -webkit-line-break: after-white-space; "><b>bold text</b><div><br></div><div><blockquote type="cite">multiline</blockquote><blockquote type="cite">quoted</blockquote><blockquote type="cite">text</blockquote></div><div><br></div><div>--</div><div>Signature block</div><div></div></body></html>)
 
           # Attachments
           message.attachments[0].filename.should == 'foo.txt'
-          message.attachments[0].read.should == "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed fringilla consectetur rhoncus. Nunc mattis mattis urna quis molestie. Quisque ut mattis nisl. Donec neque felis, porta quis condimentum eu, pharetra non libero.\n"
+          message.attachments[0].read.should == "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed fringilla consectetur rhoncus. Nunc mattis mattis urna quis molestie. Quisque ut mattis nisl. Donec neque felis, porta quis condimentum eu, pharetra non libero."
           message.attachments[1].filename.should == 'bar.txt'
-          message.attachments[1].read.should == "Nam accumsan euismod eros et rhoncus. Phasellus fermentum erat id lacus egestas vulputate. Pellentesque eu risus dui, id scelerisque neque. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.\n"
+          message.attachments[1].read.should == "Nam accumsan euismod eros et rhoncus. Phasellus fermentum erat id lacus egestas vulputate. Pellentesque eu risus dui, id scelerisque neque."
 
           # Extra Cloudmailin parameters
-          message['reply_plain'].value.should == 'bold text' unless http_post_format == 'raw'
+          message['reply_plain'].value.should == "bold text\r\n" unless http_post_format == 'raw'
           message['X-Mailgun-Spf'].value.should == 'pass'
         end
       end
