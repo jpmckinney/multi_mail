89567701f876df5cab0c7a381b1fc96e535c67a8
James McKinney <james@slashpoundbang.com>
2013-04-15 01:25:24 -0400

    commit broken mandrill spec

--- spec/mandrill/receiver_spec.rb
+++ spec/mandrill/receiver_spec.rb
@@ -53,10 +53,31 @@ describe MultiMail::Receiver::Mandrill do
         message.parts[1].content_type.should == 'text/html; charset=UTF-8'
         message.parts[1].body.should         == %(<html><head></head><body style="word-wrap: break-word; -webkit-nbsp-mode: space; -webkit-line-break: after-white-space; "><div><b>bold text</b></div><div><br></div><div><blockquote type="cite"></blockquote></div><div><blockquote type="cite">multiline</blockquote></div><blockquote type="cite"><div>quoted</div><div>text</div></blockquote><br><div>--</div><div>Signature block</div></body></html>\n)
 
+        # Attachments
+        message.attachments[0].filename.should == 'foo.txt'
+        message.attachments[0].read.should == "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed fringilla consectetur rhoncus. Nunc mattis mattis urna quis molestie. Quisque ut mattis nisl. Donec neque felis, porta quis condimentum eu, pharetra non libero.\n"
+        message.attachments[1].filename.should == 'bar.txt'
+        message.attachments[1].read.should == "Nam accumsan euismod eros et rhoncus. Phasellus fermentum erat id lacus egestas vulputate. Pellentesque eu risus dui, id scelerisque neque. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.\n"
+
         # Extra Mandrill parameters
+        message['ts'].value.should == '1356639931'
         message['email'].value.should == 'foo+bar@govkit.org'
-        message['tags'].should be_nil
-        message['sender'].should be_nil
+        message['dkim-signed'].value.should == 'true'
+        message['dkim-valid'].value.should == 'true'
+        message['X-Mailgun-SScore'].value.should == '0'
+        message['X-Mailgun-Spf'].value.should == 'pass'
+      end
+    end
+
+    describe '#spam?' do
+      it 'should return true if the response is spam' do
+        message = @service.transform(params('spam'))[0]
+        @service.spam?(message).should == true
+      end
+
+      it 'should return false if the response is ham' do
+        message = @service.transform(params('valid'))[0]
+        @service.spam?(message).should == false
       end
     end
   end
