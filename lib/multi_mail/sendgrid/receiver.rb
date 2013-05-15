3db9380aed39ea72e6b64e0b84d3bc6e829d3d61
agarrow <alexio2@mac.com>
2013-05-15 16:19:25 -0400

    begin work on sendgrid

--- lib/multi_mail/sendgrid/receiver.rb
+++ lib/multi_mail/sendgrid/receiver.rb
@@ -8,32 +8,37 @@ module MultiMail
       recognizes :http_post_format
       attr_reader :http_post_format
 
-      def initialize(options = {})
-        super
-        @sendgrid_username = options[:sendgrid_username]
-        @sendgrid_password = options[:sendgrid_password]
-        @http_post_format = options[:http_post_format]
-      end
-
       def transform(params)
-        attachments = 1.upto(params['attachments'].to_i).map do |num|
-          attachment_from_params(params["attachment#{num}"])
-        end
+        message = Mail.new do 
 
-        @message = Mail.new do
-          header params['headers']
+          headers params['headers']
+
+          # The following are redundant with `with params['headers']
+          #
+          # from    params['from']
+          # sender  params['sender']
+          # to      params['recipient']
+          # subject params['subject']
+          envelope params['envelope']
+          subject params['subject']
+
+
+          1.upto(params['attachments']) do |i|
+            key = 'attachment#{i}'
+            add_file(:filename => params[key]['Name'], :content => params[key]['Content'])
+          end
+
 
           body params['text']
-
           html_part do
             content_type 'text/html; charset=UTF-8'
             body params['html']
           end if params['html']
+        end        
+      end
 
-          attachments.each do |attachment|
-            add_file(attachment)
-          end
-        end
+      def spam?
+        
       end
     end
   end
