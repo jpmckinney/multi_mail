190fc1c1fafcaa00720a01acd054ae3c87a69ac8
James McKinney <james@slashpoundbang.com>
2013-07-01 16:41:55 -0400

    add sugar to sender base

--- lib/multi_mail/sender/base.rb
+++ lib/multi_mail/sender/base.rb
@@ -1,5 +1,3 @@
-require 'mail/check_delivery_params'
-
 module MultiMail
   module Sender
     # Abstract class for outgoing email services.
@@ -8,7 +6,7 @@ module MultiMail
     module Base
       def self.included(subclass)
         subclass.class_eval do
-          include Mail::CheckDeliveryParams
+          extend MultiMail::Sender::Base::ClassMethods
         end
       end
 
@@ -19,6 +17,29 @@ module MultiMail
       # @param [Mail::Message] mail a message
       def deliver!(mail)
         raise NotImplementedError
+      end
+
+      # @see https://github.com/wildbit/postmark-gem/blob/master/lib/postmark/message_extensions/mail.rb
+      module ClassMethods
+        def html?(message)
+          !!message.content_type && message.content_type.include?('text/html')
+        end
+
+        def html_part(message)
+          if message.html_part
+            message.html_part.body.decoded
+          elsif html?(message)
+            message.body.decoded
+          end
+        end
+
+        def text_part(message)
+          if message.text_part
+            message.text_part.body.decoded
+          elsif !html?(message)
+            message.body.decoded
+          end
+        end
       end
     end
   end
--- spec/sender/base_spec.rb
+++ spec/sender/base_spec.rb
@@ -1,37 +1,115 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
+# @see https://github.com/mikel/mail/blob/master/lib/mail/network/delivery_methods/test_mailer.rb
+class TestMailer
+  include MultiMail::Sender::Base
+
+  def initialize(options = {})
+  end
+
+  def self.deliveries
+    @deliveries ||= []
+  end
+
+  def deliver!(mail)
+    self.class.deliveries << mail
+  end
+end
+
 describe MultiMail::Sender::Base do
-  before :all do
-    TestMailer = Class.new(MultiMail::Service) do
+  let :klass do
+    Class.new do
       include MultiMail::Sender::Base
+    end
+  end
 
-      def initialize(values)
-        self.settings = values.dup
+  let :message do
+    Mail.new do
+      from    'foo@example.com'
+      to      'bar@example.com'
+      subject 'test'
+      body    'hello'
+    end
+  end
+
+  let :html_message do
+    Mail.new do
+      from    'foo@example.com'
+      to      'bar@example.com'
+      subject 'test'
+      body    '<p>hello</p>'
+      content_type 'text/html; charset=UTF-8'
+    end
+  end
+
+  let :html_and_text_message do
+    Mail.new do
+      from    'foo@example.com'
+      to      'bar@example.com'
+      subject 'test'
+
+      text_part do
+        body 'hello'
       end
 
-      def deliver!(mail)
-        check_delivery_params(mail)
-        self.class.deliveries << mail
-      end
-
-      def self.deliveries
-        @deliveries ||= []
+      html_part do
+        content_type 'text/html; charset=UTF-8'
+        body '<p>hello</p>'
       end
     end
   end
 
-  describe '#deliver!' do
+  describe '#deliver' do
     it 'should deliver a message' do
-      expect{
-        message = Mail.new do
-          delivery_method TestMailer
-          to 'foo@example.com'
-          from 'bar@example.com'
-          subject 'test'
-          body 'hello'
-        end
-        message.deliver
-      }.to change(TestMailer.deliveries, :size).by(1)
+      TestMailer.deliveries.clear
+
+      message.delivery_method TestMailer
+      message.deliver
+
+      TestMailer.deliveries.size.should == 1
+      TestMailer.deliveries.first.should == message
+    end
+  end
+
+  describe '#html?' do
+    it 'should return false if the message is text only' do
+      klass.html?(message).should == false
+    end
+
+    it 'should return true if the message is HTML only' do
+      klass.html?(html_message).should == true
+    end
+
+    it 'should return false if the message has both HTML and text parts' do
+      klass.html?(html_and_text_message).should == false
+    end
+  end
+
+  describe '#html_part' do
+    it 'should not return the body if the message is text only' do
+      klass.html_part(message).should be_nil
+    end
+
+    it 'should return the body if the message is HTML only' do
+      klass.html_part(html_message).should == '<p>hello</p>'
+    end
+
+    it 'should return the body if the message has both HTML and text parts' do
+      klass.html_part(html_and_text_message).should == '<p>hello</p>'
+    end
+  end
+
+  describe '#text_part' do
+    it 'should return the body if the message is text only' do
+      klass.text_part(message).should == 'hello'
+    end
+
+    it 'should return not the body if the message is HTML only' do
+      klass.text_part(html_message).should be_nil
+    end
+
+    it 'should return the body if the message has both HTML and text parts' do
+      klass.text_part(html_and_text_message).should == 'hello'
     end
   end
 end
