fe49576cc7fa90804837b5d65e05ad406e8fea6e
James McKinney <james@slashpoundbang.com>
2016-07-31 19:52:38 -0400

    Fix skipping of Mandrill tests

--- spec/mandrill/message_spec.rb
+++ spec/mandrill/message_spec.rb
@@ -1,6 +1,6 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
-describe MultiMail::Message::Mandrill, skip: true do
+xdescribe MultiMail::Message::Mandrill do
   let :message do
     headers = {
       'X-Autoreply'  => true,
